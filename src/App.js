eba69be2aeb577450bbb06678f318689149787cf
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-19 19:25:01 +0600

    Login, Registration, Sign up, Sign in, project setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {getAuth} from 'firebase/auth';
-import {app} from './firebase/firebase.init';
+
 import './App.css';
+import app from './firebase/firebase.init';
 
 const auth = getAuth(app);
 
@@ -8,7 +9,12 @@ const auth = getAuth(app);
 function App() {
   return (
     <div className="App">
-
+      <h1>React Firebase Auth</h1>
+      <form>
+        <input type="email" placeholder="Email" />
+        <br />
+        <input type="password" placeholder="Password" />
+      </form>
     </div>
   );
 }
---
+++ src/steps.js
@@ -0,0 +1,9 @@
+/*
+Auth Terminology and Concepts
+=============================
+
+"Register" or "Sign Up" === [First time Create an account]
+
+"log in" or "Sign IN" ==== sign in [Already have an account? Sign in]
+
+*/
